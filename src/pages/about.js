c15fda3da8ccdfe17c5440656939d0969b703247
Robin Punnoose <robin.punnoose@protonmail.com>
2022-10-05 16:48:03 -0500

    added Layout component to each page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function About() {
   return (
     <div>
+      <Layout>
         <h1>About Page</h1>
         <p>Adipisicing cupidatat cillum tempor dolore et enim pariatur quis.</p>
         <p>Adipisicing cupidatat cillum tempor dolore et enim pariatur quis.</p>
         <p>Adipisicing cupidatat cillum tempor dolore et enim pariatur quis.</p>
+      </Layout>
     </div>
   )
 }
